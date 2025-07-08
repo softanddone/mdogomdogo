7ef260c2ecd6880dd59ee73609540772b36cbd2e
Nixon <coolprojectsonly@gmail.com>
2025-07-08 12:35:01 +0300

    initial commit

---
+++ src/data/products.js
@@ -0,0 +1,16 @@
+export const products = [
+  {
+    id: 1,
+    name: "Samsung A14",
+    image: "/phones/phone1.jpg",
+    deposit: "Ksh 2,000",
+    totalPrice: "Ksh 14,999"
+  },
+  {
+    id: 2,
+    name: "Infinix Smart 7",
+    image: "/phones/phone1.jpg",
+    deposit: "Ksh 1,800",
+    totalPrice: "Ksh 12,999"
+  },
+];
