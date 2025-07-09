4ea1416003bf3441d458e42c158db5ae136c1e67
Nixon <bachnica@gmail.com>
2025-07-09 17:33:27 +0300

    shop

--- src/data/products.js
+++ src/data/products.js
@@ -1,16 +1,111 @@
 export const products = [
   {
     id: 1,
-    name: "Samsung A14",
+    name: "Samsung A16",
+    status:"new",
     image: "/phones/phone1.jpg",
-    deposit: "Ksh 2,000",
+    from:"Ksh 6000",
+    deposit: "Ksh 5499",
+    daily:"95",
     totalPrice: "Ksh 14,999"
   },
   {
     id: 2,
-    name: "Infinix Smart 7",
+    name: "Samsung A06",
     image: "/phones/phone1.jpg",
-    deposit: "Ksh 1,800",
+     status:"new",
+     from:"Ksh 4200",
+    deposit: "Ksh 3500",
+    daily:"93",
+    totalPrice: "Ksh 12,999"
+
+  },
+{
+      id: 3,
+    name: "Samsung A05",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 4000",
+    deposit: "Ksh 3500",
+     status:"new",
+    daily:"83",
     totalPrice: "Ksh 12,999"
   },
+
+  {
+    id: 4,
+    name: "X2",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 4000",
+    deposit: "Ksh 3299",
+     status:"new",
+    daily:"72",
+    totalPrice: "Ksh 12,999"
+  },
+
+    {
+    id: 5,
+    name: "X20",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 4800",
+    deposit: "Ksh 4199",
+    daily:"90",
+     status:"new",
+    totalPrice: "Ksh 12,999"
+  },
+
+      {
+    id: 6,
+    name: "M10",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 3500",
+    deposit: "Ksh 2799",
+    daily:"62",
+     status:"new",
+    totalPrice: "Ksh 12,999"
+  },
+
+        {
+    id: 7,
+    name: "Samsung A04e",
+    status:"refurb",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 2500",
+    deposit: "Ksh 1999",
+    daily:"49",
+    totalPrice: "Ksh 12,999"
+  },
+
+       {
+    id: 8,
+    name: "Samsung A05",
+    status:"refurb",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 3400",
+    deposit: "Ksh 2199",
+    daily:"50",
+    totalPrice: "Ksh 12,999"
+  },
+
+       {
+    id: 9,
+    name: "X2",
+    status:"refurb",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 3200",
+    deposit: "Ksh 2499",
+    daily:"59",
+    totalPrice: "Ksh 12,999"
+  },
+
+         {
+    id: 10,
+    name: "X2",
+    status:"refurb",
+    image: "/phones/phone1.jpg",
+    from:"Ksh 3800",
+    deposit: "Ksh 3199",
+    daily:"78",
+    totalPrice: "Ksh 12,999"
+  },
+
 ];
