0bfaa8b659f84007dffd41106dd512946e0a5fe0
softanddone <nixonbarasa18@gmail.com>
2025-07-17 16:03:34 +0300

    info

--- src/data/products.js
+++ src/data/products.js
@@ -1,17 +1,17 @@
 export const products = [
-  {
-    id: 1,
-    name: "Samsung A16",
-    status:"new",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 6000",
-    deposit: "Ksh 5499",
-    daily:"95",
-    totalPrice: "Ksh 14,999",
-    source:"/phones/Samsung-A16.webp",
-    discount:"-12%",
-    display:"block"
-  },
+  // {
+  //   id: 1,
+  //   name: "Samsung A16",
+  //   status:"new",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 6000",
+  //   deposit: "Ksh 5499",
+  //   daily:"95",
+  //   totalPrice: "Ksh 14,999",
+  //   source:"/phones/Samsung-A16.webp",
+  //   discount:"-12%",
+  //   display:"block"
+  // },
   {
     id: 2,
     name: "Samsung A06",
@@ -41,104 +41,104 @@ export const products = [
      display:"block"
   },
 
-  {
-    id: 4,
-    name: "X2",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 4000",
-    deposit: "Ksh 3299",
-     status:"new",
-    daily:"72",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Mkopa-X2-Atmos-Blue-1024x1024.webp",
-      discount:"-10%",
-      display:"none"
+  // {
+  //   id: 4,
+  //   name: "X2",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 4000",
+  //   deposit: "Ksh 3299",
+  //    status:"new",
+  //   daily:"72",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Mkopa-X2-Atmos-Blue-1024x1024.webp",
+  //     discount:"-10%",
+  //     display:"none"
 
     
-  },
+  // },
 
-    {
-    id: 5,
-    name: "X20",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 4800",
-    deposit: "Ksh 4199",
-    daily:"90",
-     status:"new",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Mkopa-X20-Purple-1024x1024.webp",
-      discount:"-11%",
-      display:"block"
-  },
+  //   {
+  //   id: 5,
+  //   name: "X20",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 4800",
+  //   deposit: "Ksh 4199",
+  //   daily:"90",
+  //    status:"new",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Mkopa-X20-Purple-1024x1024.webp",
+  //     discount:"-11%",
+  //     display:"block"
+  // },
 
-      {
-    id: 6,
-    name: "M10",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 3500",
-    deposit: "Ksh 2799",
-    daily:"62",
-     status:"new",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/m10.webp",
-      discount:"-13%",
-      display:"none"
-  },
+  //     {
+  //   id: 6,
+  //   name: "M10",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 3500",
+  //   deposit: "Ksh 2799",
+  //   daily:"62",
+  //    status:"new",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/m10.webp",
+  //     discount:"-13%",
+  //     display:"none"
+  // },
 
-        {
-    id: 7,
-    name: "Samsung A04e",
-    status:"refurb",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 2500",
-    deposit: "Ksh 1999",
-    daily:"49",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Samsung-Galaxy-A04e-Black-1024x1024.webp",
-      discount:"-13%",
-      display:"none"
-  },
+  //       {
+  //   id: 7,
+  //   name: "Samsung A04e",
+  //   status:"refurb",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 2500",
+  //   deposit: "Ksh 1999",
+  //   daily:"49",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Samsung-Galaxy-A04e-Black-1024x1024.webp",
+  //     discount:"-13%",
+  //     display:"none"
+  // },
 
-       {
-    id: 8,
-    name: "Samsung A05",
-    status:"refurb",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 3400",
-    deposit: "Ksh 2199",
-    daily:"50",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Samsung-Galaxy-A05-Silver-1024x1024.webp",
-      discount:"-14%",
-      display:"none"
-  },
+  //      {
+  //   id: 8,
+  //   name: "Samsung A05",
+  //   status:"refurb",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 3400",
+  //   deposit: "Ksh 2199",
+  //   daily:"50",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Samsung-Galaxy-A05-Silver-1024x1024.webp",
+  //     discount:"-14%",
+  //     display:"none"
+  // },
 
-       {
-    id: 9,
-    name: "X2",
-    status:"refurb",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 3200",
-    deposit: "Ksh 2499",
-    daily:"59",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Mkopa-X2-Atmos-Blue-1024x1024.webp",
-      discount:"-10%",
-      display:"none"
-  },
+  //      {
+  //   id: 9,
+  //   name: "X2",
+  //   status:"refurb",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 3200",
+  //   deposit: "Ksh 2499",
+  //   daily:"59",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Mkopa-X2-Atmos-Blue-1024x1024.webp",
+  //     discount:"-10%",
+  //     display:"none"
+  // },
 
-         {
-    id: 10,
-    name: "X20",
-    status:"refurb",
-    image: "/phones/phone1.jpg",
-    from:"Ksh 3800",
-    deposit: "Ksh 3199",
-    daily:"78",
-    totalPrice: "Ksh 12,999",
-     source:"/phones/Mkopa-X20-Purple-1024x1024.webp",
-     discount:"-14%",
-     display:"block"
-  },
+  //        {
+  //   id: 10,
+  //   name: "X20",
+  //   status:"refurb",
+  //   image: "/phones/phone1.jpg",
+  //   from:"Ksh 3800",
+  //   deposit: "Ksh 3199",
+  //   daily:"78",
+  //   totalPrice: "Ksh 12,999",
+  //    source:"/phones/Mkopa-X20-Purple-1024x1024.webp",
+  //    discount:"-14%",
+  //    display:"block"
+  // },
 
 ];
