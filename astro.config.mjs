d7ec668361dae70d71e68d2439baf0819777af69
md <nixonbarasa18@gmail.com>
2025-09-23 18:08:04 +0300

    j

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+// // @ts-check
+// import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+// // https://astro.build/config
+// export default defineConfig({});
+
+import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
+
+export default defineConfig({
+  site: 'https://mdogomdogodeals.co.ke',
+  integrations: [sitemap()],
+});
