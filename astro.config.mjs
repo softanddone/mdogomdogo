65c138fbe385d8b77917f0412369a31f6db871e6
md <nixonbarasa18@gmail.com>
2025-10-30 22:40:24 +0300

    j

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,35 @@
 // // https://astro.build/config
 // export default defineConfig({});
 
+// import { defineConfig } from 'astro/config';
+// import sitemap from '@astrojs/sitemap';
+
+// export default defineConfig({
+//   site: 'https://mdogomdogodeals.co.ke',
+//   integrations: [sitemap()],
+//   compressHTML: true,
+// });
+
+
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://mdogomdogodeals.co.ke',
-  integrations: [sitemap()],
+  integrations: [
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+      customPages: [
+        'https://www.mdogomdogodeals.co.ke/iphone-17-pro-max-vs-samsung-25-ultra',
+        'https://www.mdogomdogodeals.co.ke/product/samsung-galaxy-a07'
+      ]
+    }),
+    partytown({ config: { forward: ['dataLayer.push'] }})
+  ],
+  compressHTML: true,
+  build: { inlineStylesheets: 'auto' }
 });
+
